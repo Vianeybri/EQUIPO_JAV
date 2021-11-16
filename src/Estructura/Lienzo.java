7a8faa6344a619c1dd00d78212b131cd8f642e84
JGA_G <JGA_G@DESKTOP-H9BDN9M>
2021-11-15 23:05:16 -0600

    Se sgrego clase Lienzo.

--- src/Estructura/Lienzo.java
+++ src/Estructura/Lienzo.java
@@ -1,3 +1,5 @@
+package Estructura;
+
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -8,16 +10,6 @@ import java.util.Vector;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Alfre
- */
 public class Lienzo extends JPanel implements MouseListener,MouseMotionListener {
     private Vector<Nodo> vectorNodos;
     private Vector<Enlace> vectorEnlaces;
